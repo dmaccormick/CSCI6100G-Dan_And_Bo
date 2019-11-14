4687b68b557ad0095dd9da011289f990c4969429
Daniel MacCormick <dmaccormick@hotmail.com>
2019-11-13 23:16:02 -0500

    Added label generation and download
    The labels generate and download as a CSV. Hooked up to the buttons so the auto-movement script already presses them. CSV contains all of the info for a single batch of data, all at once.

--- Data Generation/Button Generation JS/JS/StyleGenerator.js
+++ Data Generation/Button Generation JS/JS/StyleGenerator.js
@@ -4,50 +4,65 @@ const buttonObject = document.getElementById('btn');
 // Randomize the button styling information
 GenerateNewButtonStyle = () => 
 {
+    // Create a new label object so we can hold onto the label info for later
+    const labelInfo = new LabelInfo();
+
     // Base Colour
     const colourR = Math.floor(Math.random() * 256);
     const colourG = Math.floor(Math.random() * 256);
     const colourB = Math.floor(Math.random() * 256);
     const colourA = Math.random(); // Alpha is on a range from 0 - 1
     buttonObject.style.setProperty("background", `rgba(${colourR},${colourG},${colourB},${colourA})`);
+    labelInfo.SetBackgroundColour(colourR, colourG, colourB, colourA);
 
     // Border width
     const borderWidth = Math.floor(Math.random() * 10);
     buttonObject.style.setProperty("border-width", `${borderWidth}px`)
+    labelInfo.SetBorderWidth(borderWidth);
 
     // Border colour
     const borderColourR = Math.floor(Math.random() * 256);
     const borderColourG = Math.floor(Math.random() * 256);
     const borderColourB = Math.floor(Math.random() * 256);
     buttonObject.style.setProperty("border-color", `rgb(${borderColourR},${borderColourG},${borderColourB})`);
+    labelInfo.SetBorderColour(borderColourR, borderColourG, borderColourB);
 
     // Border style
     const borderStyles = ["solid", "dashed", "inset", "dotted", "outset", "ridge", "double", "groove", "none"];
     const borderStyleIndex = Math.floor(Math.random() * borderStyles.length);
-    buttonObject.style.setProperty("border-style", `${borderStyles[borderStyleIndex]}`);
+    const borderStyle = borderStyles[borderStyleIndex];
+    buttonObject.style.setProperty("border-style", `${borderStyle}`);
+    labelInfo.SetBorderStyle(borderStyle);
 
     // Font size
     const fontSize = Math.floor(Math.random() * 60) + 10; // Plus 1 prevents invisible text
     buttonObject.style.setProperty("font-size", `${fontSize}px`);
+    labelInfo.SetFontSize(fontSize);
 
     // Padding
-    const paddingSize = Math.floor(Math.random() * 30);
-    buttonObject.style.setProperty("padding", `10px`);
+    //const paddingSize = Math.floor(Math.random() * 30);
+    //buttonObject.style.setProperty("padding", `10px`);
 
     // Border Radius
     const borderRadius = Math.floor(Math.random() * 50);
     buttonObject.style.setProperty("border-radius", `${borderRadius}px`);
+    labelInfo.SetBorderRadius(borderRadius);
 
     // Shadow size (on the y axis only)
     const shadowSizeY = Math.floor(Math.random() * 10);
+    labelInfo.SetShadowWidth(shadowSizeY);
     
     // Shadow colour
     const shadowColourR = Math.floor(Math.random() * 256);
     const shadowColourG = Math.floor(Math.random() * 256);
     const shadowColourB = Math.floor(Math.random() * 256);
+    labelInfo.SetShadowColour(shadowColourR, shadowColourG, shadowColourB);
 
     // Apply both shadow size and colour
     buttonObject.style.setProperty("box-shadow", `0px ${shadowSizeY}px rgb(${shadowColourR},${shadowColourG},${shadowColourB})`);
+
+    // Add the label information to the list stored in LabelGenerator.js
+    AddLabel(labelInfo);
 }
 
 // Create a style tag and put all of the styling there instead
