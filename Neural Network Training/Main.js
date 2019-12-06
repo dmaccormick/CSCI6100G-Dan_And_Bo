a34ccf240e14d1d113569ec95039b22e16d9e8b8
Daniel <MacCormick>
2019-12-06 18:34:12 -0500

    Fixed issues with the neural net class, hooked it up to main
    - Currently only connected to the background colour nn but will next set up the others

---
+++ Neural Network Training/Main.js
@@ -0,0 +1,159 @@
+//--- Constant Data ---//
+const IMAGE_FOLDER_PATH = 'data/';
+const FILENAME_BASE = 'Button_Img_ (';
+const NUM_IMAGES_TOTAL = 100;
+const TRAINING_PERCENTAGE = 0.8;
+const LAST_TRAINING_INDEX = TRAINING_PERCENTAGE * NUM_IMAGES_TOTAL;
+
+
+
+//--- Variable Data ---//
+let neuralNets = [];
+let imageList = [];
+let labelObjects = [];
+
+
+
+//--- p5 Callbacks ---//
+function preload()
+{
+    // Output a feedback message
+    console.log('Preloading ...');
+
+    // Clear the saved info
+    neuralNets = [];
+    imageList = [];
+    labelObjects = [];
+
+    // Initialize the neural net list
+    neuralNets.push(new NeuralNet('nn_bg_colour', '_backgroundColour', 6));
+
+    // Load all of the images in
+    LoadImages();
+}
+
+function setup()
+{
+    // Output a feedback message
+    console.log('Setting up ...');
+
+    // Initialize all of the neural networks
+    neuralNets.forEach(nn => nn.Initialize());
+}
+
+
+
+//--- UI Callbacks ---//
+const OnLabelCSVLoaded = () => 
+{
+    // Clear the existing labels
+    labelObjects = [];
+
+    // Grab the file object from the HTML
+    const fileElement = document.getElementById('labelCSV');
+
+    // Read in the entire file
+    ReadEntireFile(fileElement, function (fileContents) 
+    {
+        // Split the CSV into lines and drop the last one
+        const csvLines = fileContents.split('\n');
+        csvLines.pop();
+
+        // Parse each line into a label object and save it to the array
+        for (let i = 0; i < csvLines.length; i++) {
+            labelObjects.push(new LabelObject(csvLines[i]));
+        }
+
+        // Enable the next button
+        document.getElementById('giveImagesButton').disabled = false;
+
+        // Output a feedback message
+        console.log(`Label CSV loaded. Found [${labelObjects.length}] labels...`);
+    });
+};
+
+const OnGiveImages = () =>
+{
+    // Output a feedback message
+    console.log('Giving images to neural nets...');
+
+    // Split the image and label lists so we only grab the first x% for training. The rest are for testing
+    const trainingImages = imageList.slice(0, LAST_TRAINING_INDEX); 
+    const trainingLabels = labelObjects.slice(0, LAST_TRAINING_INDEX);
+
+    // Give all of the training images to the neural nets
+    neuralNets.forEach(nn => nn.AddImages(trainingImages, trainingLabels));
+
+    // Enable the next button
+    document.getElementById('trainButton').disabled = false;
+};
+
+const OnTrain = () =>
+{
+    // Output a feedback message
+    console.log('Training neural nets...');
+
+    // Tell all of the neural nets to begin the training process
+    neuralNets.forEach(nn => nn.Train(true));
+
+    // Enable the next button
+    document.getElementById('classifyButton').disabled = false;
+};
+
+const OnTestClassify = () =>
+{
+    // Output a feedback message
+    console.log('Testing neural nets...');
+
+    // Split the image and label lists so we get the set reserved for testing
+    const testingImages = imageList.slice(LAST_TRAINING_INDEX, NUM_IMAGES_TOTAL);
+    const testingLabels = labelObjects.slice(LAST_TRAINING_INDEX, NUM_IMAGES_TOTAL);
+
+    // Give the images to all of the neural nets and have them attempt to classify
+    neuralNets.forEach(nn => nn.TestClassify(testingImages, testingLabels, true));
+
+    // Enable the next button
+    document.getElementById('accuracyButton').disabled = false;
+};
+
+const OnOutputAccuracy = () =>
+{
+    // Output a feedback message
+    console.log('Outputting neural net accuracies...');
+
+    // Tell all of the neural nets to output their accuracies
+    neuralNets.forEach(nn => nn.OutputAccuracy());
+};
+
+
+
+//--- Utility Methods ---//
+const LoadImages = () =>
+{
+    // Output a feedback message
+    console.log('Loading images ...');
+
+    // Find the div in the HTML where the image elements will get placed
+    const imgDiv = document.getElementById('imgDiv'); 
+
+    // Load in as many images as expected
+    for (let i = 0; i < NUM_IMAGES_TOTAL; i++)
+    {
+        // Determine the full file name (ex: data/Button_Img_(1576).png)
+        // The images start at 1 so that's why i is increased by 1
+        const fullFilePath = `${IMAGE_FOLDER_PATH}${FILENAME_BASE}${i + 1}).png`; 
+
+        // Create a new img object in the HTML which uses the file as the source image
+        const newImgObj = document.createElement('img');
+        newImgObj.src = fullFilePath;
+
+        // Add the image to the HTML
+        imgDiv.appendChild(newImgObj);
+
+        // Give the img object to the imageList so we can later pass it to the NNs
+        imageList.push(newImgObj);
+    }
+
+    // Output a feedback message
+    console.log('Images loaded ...');
+};
--- Neural Network Training/NeuralNet.js
+++ Neural Network Training/NeuralNet.js
@@ -8,21 +8,21 @@ class NeuralNet
     /**
      * @constructs Neural_Net
      * 
-     * @param {string} name The name of the neural net. This will be used to help identify them AND will be the file name when saved. Should ideally match the feature it is supposed to look for
+     * @param {string} name The name of the neural net. This will be used to help identify them AND will be the file name when saved
+     * @param {string} labelVarName The name of the label variable this nn looks for. This MUST match the name of the variable in the LabelObject! (ex: '_backgroundColour', '_borderStyle', etc)
      * @param {Number} uniqueLabelCount The number of outputs for this neural net. This should equal the number of unique possible labels
      */
-    constructor(name, uniqueLabelCount)
+    constructor(name, labelVarName, uniqueLabelCount)
     {
         // Identify possible errors
         console.assert(typeof(name) === 'string', 'the neural net name must be a string');
+        console.assert(typeof(labelVarName) === 'string', 'the labelVarName must be a string and MUST match the name of the variable in the LabelObject! (ex: _backgroundColour, _borderStyle, etc)');
         console.assert(typeof(uniqueLabelCount) === 'number', 'the uniqueLabelCount must be a number');
 
         // Store the data
         this._name = name;
+        this._featureName = labelVarName;
         this._labelCount = uniqueLabelCount;
-
-        // Set up the neural network with the options provided
-        Initialize();
     }
 
 
@@ -34,7 +34,7 @@ class NeuralNet
     Initialize()
     {
         // Handle possible errors
-        console.assert(this._name !== undefined && this._labelCount !== undefined, 'you must construct the neural net before calling Initialize()');
+        console.assert(this._name !== undefined && this._featureName !== undefined && this._labelCount !== undefined, 'you must construct the neural net before calling Initialize()');
 
         // Define the nn hyperparams. All of the neural nets will use the same ones, with the only difference being the number of labels
         const nnOptions = {
@@ -45,11 +45,14 @@ class NeuralNet
             numLabels: this._labelCount, 
         };
 
+        // Cannot pass 'this' to the callback below but we can pass values instead 
+        const nameValue = this._name;
+
         // Setup the feature extractor
-        this._featureExtractor = ml5._featureExtractor("MobileNet", nnOptions, this.OnModelReady);
+        this._featureExtractor = ml5.featureExtractor("MobileNet", nnOptions, function(){console.log(`[${nameValue}] model ready`);});
 
         // Setup the classifier object
-        this._classifier = featureExtractor.classification();
+        this._classifier = this._featureExtractor.classification();
 
         // Setup the promises list. These are all promises that will need to be resolved when adding data to the model
         this._promises = [];
@@ -63,25 +66,24 @@ class NeuralNet
      * @param {LabelObject[]} labels The list of labels for the data
      * @param {string} labelName The name of the specific label to grab from the label object (ex: '_background' or '_borderWidth')
      */
-    AddImages(images, labels, labelName)
+    AddImages(images, labels)
     {
         // Identify possible errors
         console.assert(Array.isArray(images), 'images must be an array');
         console.assert(Array.isArray(labels), 'labels must be an array');
         console.assert(images.length === labels.length, 'there must be an equal number of images and labels');
-        console.assert(typeof(labelName) == 'string', 'labelName must be a string');
 
         // Clear any previous promises from other times
-        promises = [];
+        this._promises = [];
 
         // Loop through all of the images and labels and add them to the internal model
         for (let i = 0; i < images.length && i < labels.length; i++)
         {
             // Grab the label value for this button and this neural net's specific target feature
-            const imageLabel = labels[i][labelName];
+            const imageLabel = labels[i][this._featureName];
 
             // Pass the image to the network using a Promise structure
-            promises.push(new Promise(resolve => 
+            this._promises.push(new Promise(resolve => 
             {
                 // Grab the image object
                 let imageObject = images[i];
@@ -103,13 +105,16 @@ class NeuralNet
     Train(outputProgress = true)
     {
         // Identify possible errors
-        console.assert(outputProgress instanceof Boolean, 'outputProgress must be a Boolean');
+        console.assert(typeof(outputProgress) === 'boolean', 'outputProgress must be a Boolean');
+
+        // Cannot pass 'this' to the callback below but we can pass values instead 
+        const nameValue = this._name;
 
         // Train the network
         this._classifier.train(function (lossValue) 
         {
             // Output the loss value if told to do so
-            console.log(`[${this._name}] Loss = ${lossValue}`);
+            console.log(`[${nameValue}] Loss = ${lossValue}`);
         });
     }
 
@@ -118,20 +123,24 @@ class NeuralNet
      * 
      * @param {HTMLImageElement[]} images The list of images to try and classify
      * @param {LabelObject[]} labels The list of labels for the images. Used to determine if the guesses were correct
-     * @param {string} labelName The name of the specific feature's label within the LabelObject structure
      * @param {Boolean} outputAllGuesses (OPTIONAL) Write out all of the guesses to the console
      */
-    TestClassify(images, labels, labelName, outputAllGuesses = true)
+    TestClassify(images, labels, outputAllGuesses = true)
     {
         // Identify possible errors
         console.assert(Array.isArray(images), 'images must be an array');
         console.assert(Array.isArray(labels), 'labels must be an array');
         console.assert(images.length === labels.length, 'there must be an equal number of images and labels');
-        console.assert(typeof(labelName) == 'string', 'labelName must be a string');
+        console.assert(typeof(outputAllGuesses) === 'boolean', 'outputAllGuesses must be a Boolean');
 
         // Keep track of the number of guesses and number of attempts
         this._numGuessesCorrect = 0;
         this._numGuessesTotal = images.length;
+
+        // Cannot pass 'this' to the callback below directly but we can pass values instead 
+        const nameValue = this._name;
+        const featureNameValue = this._featureName;
+        const thisObj = this;
 
         // Attempt to classify all of the images
         for (let i = 0; i < images.length; i++)
@@ -140,7 +149,7 @@ class NeuralNet
             this._classifier.classify(images[i], function(error, results) 
             {
                 // If there was an error, output it. Otherwise, handle the result
-                if (err) 
+                if (error) 
                 {
                     // Write out the error
                     console.error(error);
@@ -148,7 +157,7 @@ class NeuralNet
                 else
                 {
                     // Get the expected label which is the correct answer
-                    const answer = labels[i][labelName];
+                    const answer = labels[i][featureNameValue];
 
                     // Get the top result which is the guess
                     const guess = results[0].label;
@@ -158,7 +167,7 @@ class NeuralNet
 
                     // Keep track of the number of correct guesses so we can determine the accuracy later
                     if (answer === guess) {
-                        this._numGuessesCorrect++;
+                        thisObj._numGuessesCorrect++;
                     }
 
                     // If desired, output the guess and result to the console
@@ -170,7 +179,7 @@ class NeuralNet
                         const correctStr = (correctGuess) ? 'CORRECT' : 'INCORRECT';
 
                         // Write out the guess information
-                        console.log(`[${this._name}] Test ${testNumber}: Guess = ${guess} Confidence = ${confidenceStr} (${correctStr}, Answer = ${answer})`);
+                        console.log(`[${nameValue}] Test ${testNumber}: Guess = ${guess} Confidence = ${confidenceStr} (${correctStr}, Answer = ${answer})`);
                     }
                 }
             });
@@ -189,21 +198,9 @@ class NeuralNet
         let accuracyInfo = `[${this._name}] Classification Test Results :\n`;
         accuracyInfo += `Attempts: ${this._numGuessesTotal}\n`;
         accuracyInfo += `Correct:  ${this._numGuessesCorrect}\n`;
-        accuracyInfo += `Accuracy: ${100 * (this._numGuessesCorrect / this._numGuessesCorrect)}%`;
+        accuracyInfo += `Accuracy: ${100 * (this._numGuessesCorrect / this._numGuessesTotal)}%`;
 
         // Write the string to the console
         console.log(accuracyInfo);
-    }
-
-
-
-    //--- Neural Net Callbacks ---//
-    /**
-     * Called when the feature extractor model is ready. Outputs a message indicating as such
-     */
-    OnModelReady()
-    {
-        // Output a message indicating that this specific model is ready
-        console.log(`Neural Net [${this._name}] is ready!`);
     }
 }
